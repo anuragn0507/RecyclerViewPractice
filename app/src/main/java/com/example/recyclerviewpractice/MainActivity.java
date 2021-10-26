7c72a0c33634eafa6297a969c502732e78f18b34
Anurag Yadav <yadavanurag307@gmail.com>
2021-10-27 04:11:45 +0530

    incomplete Recyclerview practice

--- app/src/main/java/com/example/recyclerviewpractice/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewpractice/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.recyclerviewpractice;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
 
 import java.util.ArrayList;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView cities;
+    private RecyclerView.Adapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<City> cities = initCities();
+
+        this.cities = (RecyclerView) findViewById(R.id.cities);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        this.cities.setLayoutManager(mLayoutManager);
+
+
+        adapter = new CityAdapter(cities);
+        this.cities.setAdapter(adapter);
     }
 
     private ArrayList<City> initCities () {
